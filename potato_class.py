24478181c6452530c60aeef3308b16cd791ceb7e
JoshGulab <josh.gulab@googlemail.com>
2014-06-17 14:30:18 +0100

    task 7

---
+++ potato_class.py
@@ -0,0 +1,35 @@
+from crop import *
+
+class Potato(Crop):
+    """A potato crop"""
+    # constructor
+    def __init__(self):
+        super().__init__(1,3,6)
+        self._type = "Potato"
+
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            if self._status == "Seedling" and water > self._water_need:
+                self._growth += self._growth *1.5
+            elif self._status == "Young" and water > self._water_need:
+                self._growth += self._growth_rate * 1.25
+            else:
+                self._growth += self._growth_rate
+        #increment days growing
+        self._days_growing =+ 1
+        #update the status
+        self._update_status()
+                
+        
+
+def main():
+    potato_crop = Potato()
+    print(potato_crop.report())
+    manual_grow(potato_crop)
+    print(potato_crop.report())
+    manual_grow(potato_crop)
+    print(potato_crop.report())
+
+if __name__ == "__main__":
+    main()
+    
