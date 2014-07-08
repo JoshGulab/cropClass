5b1452a6103941344c2deb457bd50be9f69d6013
JoshGulab <josh.gulab@googlemail.com>
2014-07-08 11:18:43 +0100

    task 3

---
+++ graphic_cow_item_class.py
@@ -0,0 +1,13 @@
+from graphic_animal_item_class import *
+from cow_class import *
+
+import images
+
+class CowGraphicsPixmapItem(AnimalGraphicsPixmapItem):
+    """this class porvides a graphical representation of a cow"""
+
+    #constructor
+    def __init__(self):
+        self.available_graphics = [":/cow_baby.png", ":/cow_poor.png", ":/cow_fine.png", ":/cow_prime"]
+        super().__init__(self.available_grapics)
+        self.animal = Cow("")
---
+++ graphic_sheep_item_class.py
@@ -0,0 +1,13 @@
+from graphic_animal_item_class import *
+from sheep_class import *
+
+import images
+
+class SheepGraphicsPixmapItem(AnimalGraphicsPixmapItem):
+    """this class porvides a graphical representation of a sheep"""
+
+    #constructor
+    def __init__(self):
+        self.available_graphics = [":/sheep_baby.png", ":/sheep_poor.png", ":/sheep_fine.png", ":/sheep_prime"]
+        super().__init__(self.available_grapics)
+        self.animal = Sheep("")
